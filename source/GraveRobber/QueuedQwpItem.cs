3a3bc8f3f0bea902a0d2a656121f31b5a9721484
Sam <ArcticEcho@users.noreply.github.com>
2018-04-04 19:23:50 +0100

    Let's rebuild this from scratch

--- source/GraveRobber/QueuedQwpItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GraveRobber
-{
-    internal class QueuedQwpItem
-    {
-        public int PostID { get; set; }
-        public int ActionRequestMessageID { get; set; }
-        public bool ReportIfOverThreshold { get; set; }
-    }
-}
